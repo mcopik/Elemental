6f47ddc55be392ae2be15d944621c2ec0e540075
SebastianAchilles <S.Achilles@grs-sim.de>
2016-01-25 11:55:52 +0100

    change added

---
+++ external/pmrrr_extended/change_names3.sh
@@ -0,0 +1,152 @@
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/ode2/ext_ode2/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odebz/ext_odebz2/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odev2/ext_odev2/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odnan/ext_odnan/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odneg/ext_odneg/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odnst/ext_odnst/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odr1v/ext_odr1v/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odrnv/ext_odrnv/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odrra/ext_odrra/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odrrb/ext_odrrb/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odrrc/ext_odrrc/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odrrd/ext_odrrd/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odrre/ext_odrre/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odrrf/ext_odrrf/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odrrj/ext_odrrj/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odrrk/ext_odrrk/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odrrr/ext_odrrr/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odrrv/ext_odrrv/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odruv/ext_odruv/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odset/ext_odset/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odsnan/ext_odsnan/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odsq2/ext_odsq2/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odsq3/ext_odsq3/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odsq4/ext_odsq4/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odsq5/ext_odsq5/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odsq6/ext_odsq6/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odsrt/ext_odsrt/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odssq/ext_odssq/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odstmr/ext_odstmr/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/oerbla/ext_oerbla/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/olsame/ext_olsame/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xde2/ext_xde2/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdebz/ext_xdebz/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdev2/ext_xdev2/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdnan/ext_xdnan/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdneg/ext_xdneg/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdnst/ext_xdnst/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdr1v/ext_xdr1v/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdrnv/ext_xdrnv/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdrra/ext_xdrra/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdrrb/ext_xdrrb/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdrrc/ext_xdrrc/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdrrd/ext_xdrrd/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdrre/ext_xdrre/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdrrf/ext_xdrrf/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdrrj/ext_xdrrj/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdrrk/ext_xdrrk/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdrrr/ext_xdrrr/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdrrv/ext_xdrrv/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdruv/ext_xdruv/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdset/ext_xdset/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdsnan/ext_xdsnan/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdsq2/ext_xdsq2/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdsq3/ext_xdsq3/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdsq4/ext_xdsq4/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdsq5/ext_xdsq5/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdsq6/ext_xdsq6/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdsrt/ext_xdsrt/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdssq/ext_xdssq/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdstmr/ext_xdstmr/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xlsame/ext_xlsame/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xrbla/ext_xrbla/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odscal/ext_odscal/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odcpy/ext_odcpy/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odscl/ext_odscl/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/odswap/ext_odswap/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdcpy/ext_xdcpy/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdscl/ext_xdscl/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/xdswap/ext_xdswap/g" {} \;
+
+
+#pmrrr.c
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/handle_small_cases/ext_handle_small_cases/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/cmp/ext_cmp/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/cmpb/ext_cmpb/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/scale_matrix/ext_scale_matrix/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/invscale_eigenvalues/ext_invscale_eigenvalues/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/sort_eigenpairs/ext_sort_eigenpairs/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/clean_up/ext_clean_up/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/refine_to_highrac/ext_refine_to_highrac/g" {} \;
+
+#counter.c
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/PMR_create_counter/ext_PMR_create_counter/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/PMR_destroy_counter/ext_PMR_destroy_counter/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/PMR_get_counter_value/ext_PMR_get_counter_value/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/PMR_decrement_counter/ext_PMR_decrement_counter/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/PMR_increment_counter/ext_PMR_increment_counter/g" {} \;
+
+#plarre.c
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/eigval_subset_thread_a/ext_eigval_subset_thread_a/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/eigval_subset_thread_r/ext_eigval_subset_thread_r/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/clean_up_plarre/ext_clean_up_plarre/g" {} \;
+
+#plarrv.c
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/eigval_approx_proc/ext_eigval_approx_proc/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/eigval_root_proc/ext_eigval_root_proc/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/eigval_refine_proc/ext_eigval_refine_proc/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/create_auxarg1/ext_create_auxarg1/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/retrieve_auxarg1/ext_retrieve_auxarg1/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/create_auxarg2/ext_create_auxarg2/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/retrieve_auxarg2/ext_retrieve_auxarg2/g" {} \;
+
+#process_c_task.c
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/assign_to_proc/ext_assign_to_proc/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/cmpa/ext_cmpa/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/init_workQ/ext_init_workQ/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/empty_workQ/ext_empty_workQ/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/create_workQ/ext_create_workQ/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/destroy_workQ/ext_destroy_workQ/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/create_auxarg3/ext_create_auxarg3/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/retrieve_auxarg3/ext_retrieve_auxarg3/g" {} \;
+
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/compute_new_rrr/ext_compute_new_rrr/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/refine_eigvals/ext_refine_eigvals/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/communicate_refined_eigvals/ext_communicate_refined_eigvals/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/test_comm_status/ext_test_comm_status/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/create_subtasks/ext_create_subtasks/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/PMR_process_c_task/ext_PMR_process_c_task/g" {} \;
+
+#process_r_task.c
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/PMR_process_r_task/ext_PMR_process_r_task/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/PMR_process_r_queue/ext_PMR_process_r_queue/g" {} \;
+
+#process_s_task.c
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/PMR_process_s_task/ext_PMR_process_s_task/g" {} \;
+
+#queue.c
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/PMR_create_empty_queue/ext_PMR_create_empty_queue/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/PMR_destroy_queue/ext_PMR_destroy_queue/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/PMR_insert_task_at_front/ext_PMR_insert_task_at_front/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/PMR_insert_task_at_back/ext_PMR_insert_task_at_back/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/PMR_remove_task_at_back/ext_PMR_remove_task_at_back/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/PMR_get_num_tasks/ext_PMR_get_num_tasks/g" {} \;
+
+#rrr.c
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/PMR_create_rrr/ext_PMR_create_rrr/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/PMR_reset_rrr/ext_PMR_reset_rrr/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/PMR_increment_rrr_dependencies/ext_PMR_increment_rrr_dependencies/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/PMR_set_parent_processed_flag/ext_PMR_set_parent_processed_flag/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/PMR_set_copied_parent_rrr_flag/ext_PMR_set_copied_parent_rrr_flag/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/PMR_try_destroy_rrr/ext_PMR_try_destroy_rrr/g" {} \;
+
+#tasks.c
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/PMR_create_s_task/ext_PMR_create_s_task/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/PMR_create_c_task/ext_PMR_create_c_task/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/PMR_create_r_task/ext_PMR_create_r_task/g" {} \;
+
+
+#odscal.c
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/pmext_rrr_dscal/pmrrr_dscal/g" {} \;
+find . \( -name \*.c -o -name \*.h \) -exec sed -i "s/pmrrr_xscal/pmrrr_xscal/g" {} \;
+
